dd78f35c28fa28b1d0a5d940e2c8a06c42b1e53e
srenaudot <srenaudot@E201-14>
2025-01-30 14:38:24 +0100

    first commit

---
+++ src/components/Author.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { reactive, computed } from 'vue'
+
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Brosseau',
+    'Vue 3 - Test',
+    'Vue 4 - Démo'
+  ]
+})
+
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 0 ? 'Yes' : 'No'
+})
+</script>
+
+<template>
+  <p>
+    Auteur : {{author.name}}
+  </p>
+  
+  <ul>
+    <li v-for="book in author.books">{{ book }}</li>
+  </ul>
+
+  <p>
+  Nombre de livre : <span>{{ publishedBooksMessage }}</span>
+  </p>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  {{ x }}, {{ y }}
+  <div v-if="isDark">Dark mode</div>
+  <div v-else>Light mode</div>
+</template>
+
+<script setup lang="ts">
+import { useMouse, usePreferredDark, useLocalStorage } from '@vueuse/core'
+
+  // tracks mouse position
+  const { x, y } = useMouse()
+
+  // is user prefers dark theme
+  const isDark = usePreferredDark()
+</script>
